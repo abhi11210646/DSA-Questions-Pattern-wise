f62af635534395e74b0c8f881f97d2603af23e78
Abhisekh kumar singh <abhisekhs@one.com>
2023-12-31 00:27:17 +0530

    README.md update

---
+++ Binary Search/README.md
@@ -0,0 +1,5 @@
+Binary search : O(logN)
+Input: Sorted Array[1,2,3,4,5]
+
+Modified binary search : O(logN)
+Input: Rotated Sorted Array[4,5,1,2,3]
--- Binary Search/find-minimum-in-rotated-sorted-array.js
+++ Binary Search/find-minimum-in-rotated-sorted-array.js
@@ -1,28 +1,28 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
 function findMin(nums) {
-    let l = 0, r = nums.length-1
-    if(nums[l] <= nums[r]) return nums[l];
+    let l = 0, r = nums.length - 1
+    if (nums[l] <= nums[r]) return nums[l];
 
-    while(l <= r) {
+    while (l <= r) {
         let mid = Math.floor(l + (r - l) / 2);
-        if(nums[mid] > nums[mid+1]) return nums[mid+1];
+        if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
 
-        if(nums[mid] < nums[l]) {
+        if (nums[mid] < nums[l]) {
             r = mid;
-        }else {
+        } else {
             l = mid;
         }
 
     }
 };
 
-console.log(findMin([3,4,5,1,2]))
+console.log(findMin([3, 4, 5, 1, 2]))
 console.log(findMin([1]))
-console.log(findMin([2,4,5,6,7,0,1]))
-console.log(findMin([0,1,2]))
-console.log(findMin([5,6,7,0,1,2,4]))
-console.log(findMin([7,0,1,2,4,5,6]))
+console.log(findMin([2, 4, 5, 6, 7, 0, 1]))
+console.log(findMin([0, 1, 2]))
+console.log(findMin([5, 6, 7, 0, 1, 2, 4]))
+console.log(findMin([7, 0, 1, 2, 4, 5, 6]))
 
 
 
